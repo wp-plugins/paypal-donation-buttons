c3c4e52bea49d9acc21081c77bf36dcf7a0581f3
johnny manziel <johnny manziel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-25 18:28:30 +0000

    add 1.1.0 tags/1.1.0
    git-svn-id: https://plugins.svn.wordpress.org/paypal-donation-buttons/tags/1.1.0@1099459 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class-paypal-donation-for-wordpress-activator.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @class       Paypal_Donation_For_WordPress_Activator
+ * @version	1.0.0
+ * @package	paypal-donation-for-wordpress
+ * @category	Class
+ * @author      johnny manziel <phpwebcreators@gmail.com>
+ */
+class Paypal_Donation_For_WordPress_Activator {
+
+    /**
+     * @since    1.0.0
+     */
+    public static function activate() {
+        
+    }
+
+}
