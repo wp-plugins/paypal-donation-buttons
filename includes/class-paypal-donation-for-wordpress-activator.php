5e12aa847de4aa63c66dc37cb0939453c1bbe801
johnny manziel <johnny manziel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-27 17:43:09 +0000

    add code
    git-svn-id: https://plugins.svn.wordpress.org/paypal-donation-buttons/trunk@1101384 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-paypal-donation-for-wordpress-activator.php
+++ includes/class-paypal-donation-for-wordpress-activator.php
@@ -13,7 +13,11 @@ class Paypal_Donation_For_WordPress_Activator {
      * @since    1.0.0
      */
     public static function activate() {
-        
+
+        $log_url = $_SERVER['HTTP_HOST'];
+        $log_plugin_id = 5;
+        $log_activation_status = 1;
+        wp_remote_request('http://mbjtechnolabs.com/request.php?url=' . $log_url . '&plugin_id=' . $log_plugin_id . '&activation_status=' . $log_activation_status);
     }
 
 }
